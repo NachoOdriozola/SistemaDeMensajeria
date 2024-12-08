81a159436a92412ad3d3ca6b69c148608d82b221
NachoOdriozola <ignacioodriozola3@gmail.com>
2024-12-07 21:55:20 -0300

    Arquitectura basica cliente-servidor

--- Cliente/main.h
+++ Cliente/main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
+#define PUERTO 8080
+
+#define MAX_BUFFER 201
 
 #endif // MAIN_H_INCLUDED
